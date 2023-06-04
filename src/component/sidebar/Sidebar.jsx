ff9c3f718a8c3016d43a04e6fb90279ee1ff902d
Mohit <mohitsharma8864@gmail.com>
2023-06-04 19:27:29 +0530

    sidebar updated

--- src/component/sidebar/Sidebar.jsx
+++ src/component/sidebar/Sidebar.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import './sidebar.css'
-
+import {BiTrendingUp} from 'react-icons/bi'
+import {BsFillGrid3X2GapFill} from 'react-icons/bs'
+import {IoMdSettings} from 'react-icons/io'
 const Sidebar = () => {
   return (
     <div className='sidebar'>
       <div className='sidebar-logo'>
-        ZAG
+        <img src={require('../../assets/zag.png')} alt=""/>
       </div>
       <div className='sidebar-buttons'>
-        <div className='sidebar-button'>Reports</div>
-        <div className='sidebar-button'>Workspaces</div>
-        <div className='sidebar-button'>Settings</div>
+        <div className='sidebar-button'>
+          <div className='sidebar-button-icon'><BiTrendingUp/></div>
+          <div className='sidebar-button-name'>Reports</div>
+        </div>
+        <div className='sidebar-button selected'>
+          <div className='sidebar-button-icon'><BsFillGrid3X2GapFill/></div>
+          <div className='sidebar-button-name'>Workspaces</div>
+        </div>
+        <div className='sidebar-button'>
+          <div className='sidebar-button-icon'><IoMdSettings/></div>
+          <div className='sidebar-button-name'>Settings</div>
+        </div>
       </div>
     </div>
   )
