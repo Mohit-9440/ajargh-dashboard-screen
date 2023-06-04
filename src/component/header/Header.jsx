1e12d313a3b6c3985f1327d7ef8b3c64a9f4ce88
Mohit <mohitsharma8864@gmail.com>
2023-06-04 19:27:20 +0530

    header updated

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import './header.css'
+import { IoMdAdd } from 'react-icons/io'
 const Header = () => {
   return (
     <div className='header'>
       <div className='header-div'>
           <div className='header-heading'><h1>Orders</h1></div>
-          <div className='header-button-wrap'><button>+ Add Order</button></div>
+          <div className='header-button-wrap'>
+            <button>
+              <IoMdAdd className='icon'/>
+              <p>Add Order</p>
+            </button>
+          </div>
       </div>
     </div>
   )
