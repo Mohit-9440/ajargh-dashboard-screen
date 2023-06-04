2e26236dd14ec24fc28bd314c5c5ddb2e5fae053
Mohit <mohitsharma8864@gmail.com>
2023-06-04 14:08:23 +0530

    header

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import './header.css'
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='header'>
+      <div className='header-div'>
+          <div className='header-heading'><h1>Orders</h1></div>
+          <div className='header-button-wrap'><button>+ Add Order</button></div>
+      </div>
+    </div>
   )
 }
 
