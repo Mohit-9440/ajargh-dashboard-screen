ddbc8dc2325c6b6fce7aea2b64bdb75caf18e015
Mohit <mohitsharma8864@gmail.com>
2023-06-04 14:08:10 +0530

    main section

--- src/component/main/Main.jsx
+++ src/component/main/Main.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import './main.css'
+import DataGrids from '../datagrid/Datagrid'
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <div className='main'>
+      <div className='confirmed-wrap'>
+        <div className='confirmed-head'>
+          <div className='confirmed-head-left'>
+              <p>Confirmed</p>
+              <p>258</p>
+            </div>
+            <div className='confirmed-head-right'>
+              <button>-</button>
+            </div>
+        </div>
+        <div className='confirmed-content'>
+          <DataGrids/>
+        </div>
+      </div>
+      <div className='issues-wrap'>
+        <div className='issues'>
+          <div className='issues-left'>
+            <p>Issues</p>
+            <p>21</p>
+          </div>
+          <div className='issues-right'>
+            <button>+</button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
