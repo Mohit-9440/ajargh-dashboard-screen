5f8f60ea63dedfc3879ea9f8c5e795fc368a70cf
Mohit <mohitsharma8864@gmail.com>
2023-06-05 08:37:40 +0530

    main page

--- src/component/main/Main.jsx
+++ src/component/main/Main.jsx
@@ -1,22 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './main.css'
 import DataGrids from '../datagrid/Datagrid'
 
 const Main = () => {
+
+  const [isSectionVisible, setIsSectionVisible] = useState(false);
+
+  const handleButtonClick = () => {
+    setIsSectionVisible(!isSectionVisible);
+  };
+
   return (
     <div className='main'>
       <div className='confirmed-wrap'>
         <div className='confirmed-head'>
           <div className='confirmed-head-left'>
-            <h3>Confirmed</h3>
-            <p>258</p>
+          <div className='confirmed-title'>
+              <h3>Confirmed</h3>
+              <p>258</p>
+          </div>
+          <button onClick={handleButtonClick}>
+            {isSectionVisible ? '' : ''}
+            <span>{isSectionVisible ? '-' : '+'}</span>
+          </button>
+
           </div>
           <div className='confirmed-head-right'>
-            <button>-</button>
+          {isSectionVisible && (
+            <div className='confirmed-content'>
+              <DataGrids/>
+            </div>
+          )}
           </div>
-        </div>
-        <div className='confirmed-content'>
-          <DataGrids/>
         </div>
       </div>
       <div className='issues-wrap'>
